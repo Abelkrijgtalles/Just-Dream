000fa4714487c11fe9bf74126048531a4cbef2f4
Abel van Hulst <abelvanhulst@gmail.com>
2024-09-14 20:50:55 +0200

    just goofing around

--- fabric/src/main/java/nl/abelkrijgtalles/balls/fabric/Entrypoint.java
+++ fabric/src/main/java/nl/abelkrijgtalles/balls/fabric/Entrypoint.java
@@ -1,8 +1,11 @@
 package nl.abelkrijgtalles.balls.fabric;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import nl.abelkrijgtalles.balls.Balls;
+import nl.abelkrijgtalles.balls.TestWorld;
 import nl.abelkrijgtalles.balls.fabric.util.multiloader.FabricItemHandler;
+import static net.minecraft.commands.Commands.*;
 
 public class Entrypoint implements ModInitializer {
     @Override
@@ -13,5 +16,8 @@ public class Entrypoint implements ModInitializer {
         Balls.init();
 
         FabricItemHandler.register();
+
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("worldtest")
+                .executes(TestWorld::test)));
     }
 }
