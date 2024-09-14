4df876488e15732fe42f18164bbc4a692540ece5
Abel <56084910+Abelkrijgtalles@users.noreply.github.com>
2024-09-14 15:00:42 +0200

    Initial commit

---
+++ neoforge/src/main/java/nl/abelkrijgtalles/balls/neoforge/specialentrypoints/EntrypointClient.java
@@ -0,0 +1,9 @@
+package nl.abelkrijgtalles.balls.neoforge.specialentrypoints;
+
+import nl.abelkrijgtalles.balls.InitBallsClient;
+
+public class EntrypointClient {
+    public static void init() {
+        InitBallsClient.init();
+    }
+}
---
+++ neoforge/src/main/java/nl/abelkrijgtalles/balls/neoforge/specialentrypoints/EntrypointDedicatedServer.java
@@ -0,0 +1,9 @@
+package nl.abelkrijgtalles.balls.neoforge.specialentrypoints;
+
+import nl.abelkrijgtalles.balls.InitBallsDedicatedServer;
+
+public class EntrypointDedicatedServer {
+    public static void init() {
+        InitBallsDedicatedServer.init();
+    }
+}
